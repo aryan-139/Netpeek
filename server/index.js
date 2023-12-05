2ce1d47667a6f65b023bc080fde02ef412819307
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-06 02:40:59 +0530

    basic refactoring done

--- server/index.js
+++ server/index.js
@@ -1,128 +1,74 @@
+// app.js
 const Express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan=require('morgan');
-const helmet=require('helmet');
-const NetworkSpeed = require('network-speed');
-const ip=require('ip');
-const ping=require('ping');
-
-
+const { getNetworkDownloadSpeed, getNetworkUploadSpeed } = require('./routes/networkSpeed');
+const { uploadSpeedInstance, downloadSpeedInstance, updateSpeedInstances } = require('./routes/speedInstance');
 
 const app = new Express();
 
 app.use(cors());
 
-const deviceIp=ip.address();
-//console.log(deviceIp);
-const testNetworkSpeed = new NetworkSpeed();
+const PORT = 8001;
 
-getNetworkDownloadSpeed();
-async function getNetworkDownloadSpeed() {
-    const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
-    const fileSizeInBytes = 500000;
-    const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
-    return speed;
-}
-getNetworkUploadSpeed();
-
-async function getNetworkUploadSpeed() {
-    const options = {
-      hostname: 'www.google.com',
-      port: 80,
-      path: '/catchers/544b09b4599c1d0200000289',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    const fileSizeInBytes = 2000000
-    const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-    return speed;
-  }
-
-const PORT=8001;
-const uploadSpeedInstance = [];
-const downloadSpeedInstance = [];
-
-// get last 5 speed instances for the graphs
 async function getLastTenSpeedInstance() {
-  let i = 0;
+    let i = 0;
 
-  return new Promise((resolve) => {
-    const interval = setInterval(async () => {
-      if (i === 5) {
-        console.log(uploadSpeedInstance);
-        console.log(downloadSpeedInstance);
-        clearInterval(interval);
-        resolve(); // Resolve the promise after 5 iterations
-      } else {
-        try {
-          const uploadSpeedPromise = getNetworkUploadSpeed();
-          const downloadSpeedPromise = getNetworkDownloadSpeed();
+    return new Promise((resolve) => {
+        const interval = setInterval(async () => {
+            if (i === 5) {
+                console.log(uploadSpeedInstance);
+                console.log(downloadSpeedInstance);
+                clearInterval(interval);
+                resolve(); // Resolve the promise after 5 iterations
+            } else {
+                try {
+                    const uploadSpeedPromise = getNetworkUploadSpeed();
+                    const downloadSpeedPromise = getNetworkDownloadSpeed();
 
-          const uploadSpeedResult = await uploadSpeedPromise;
-          const downloadSpeedResult = await downloadSpeedPromise;
+                    const [uploadSpeedResult, downloadSpeedResult] = await Promise.all([uploadSpeedPromise, downloadSpeedPromise]);
 
-          if (uploadSpeedInstance.length >= 5) {
-            uploadSpeedInstance.shift(); 
-          }
-          if (downloadSpeedInstance.length >= 5) {
-            downloadSpeedInstance.shift(); 
-          }
+                    updateSpeedInstances(uploadSpeedResult.mbps, downloadSpeedResult.mbps);
 
-
-          uploadSpeedInstance.push(uploadSpeedResult.mbps);
-          downloadSpeedInstance.push(downloadSpeedResult.mbps);
-
-          i++;
-        } catch (error) {
-          console.error('Error fetching speed:', error);
-        }
-      }
-    }, 1000);
-  });
+                    i++;
+                } catch (error) {
+                    console.error('Error fetching speed:', error);
+                }
+            }
+        }, 1000);
+    });
 }
-
-
 
 app.get('/speed', async (req, res) => {
-  console.log('Speed request received');
-  try {
-    await getLastTenSpeedInstance();
-    const [uploadSpeed, downloadSpeed] = await Promise.all([
-      getNetworkUploadSpeed(),
-      getNetworkDownloadSpeed(),
-    ]);
+    console.log('Speed request received');
+    try {
+        await getLastTenSpeedInstance();
+        const [uploadSpeed, downloadSpeed] = await Promise.all([getNetworkUploadSpeed(), getNetworkDownloadSpeed()]);
 
-    const speed = {
-      uploadSpeed: uploadSpeed.mbps,
-      downloadSpeed: downloadSpeed.mbps,
-      sessionData: 550,
-      nearestServer: 'Kolkata, India',
-      currentISP: 'Bharti Airtel',
-      sessionStatus: 'Inactive',
-      uploadSpeedInstance,
-      downloadSpeedInstance,
-    };
+        const speed = {
+            uploadSpeed: uploadSpeed.mbps,
+            downloadSpeed: downloadSpeed.mbps,
+            sessionData: 550,
+            nearestServer: 'Kolkata, India',
+            currentISP: 'Bharti Airtel',
+            sessionStatus: 'Inactive',
+            uploadSpeedInstance,
+            downloadSpeedInstance,
+        };
 
-    console.log(speed);
-    res.json(speed);
-  } catch (err) {
-    if (err.code === 'ECONNRESET') {
-      console.error('Connection reset by peer');
-      // Handle or log the error as needed
-      res.status(500).json({ error: 'Connection reset by peer' });
-    } else {
-      console.log(err);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.log(speed);
+        res.json(speed);
+    } catch (err) {
+        if (err.code === 'ECONNRESET') {
+            console.error('Connection reset by peer');
+            res.status(500).json({ error: 'Connection reset by peer' });
+        } else {
+            console.log(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
-  }
 });
 
-
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/routes/networkSpeed.js
@@ -0,0 +1,28 @@
+// networkSpeed.js
+const NetworkSpeed = require('network-speed');
+
+const testNetworkSpeed = new NetworkSpeed();
+
+async function getNetworkDownloadSpeed() {
+    const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
+    const fileSizeInBytes = 500000;
+    const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+    return speed;
+}
+
+async function getNetworkUploadSpeed() {
+    const options = {
+        hostname: 'www.google.com',
+        port: 80,
+        path: '/catchers/544b09b4599c1d0200000289',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const fileSizeInBytes = 2000000;
+    const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+    return speed;
+}
+
+module.exports = { getNetworkDownloadSpeed, getNetworkUploadSpeed };
