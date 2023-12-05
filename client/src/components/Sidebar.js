0ad6201ad296b9aa216b2e93da853e962f0033a8
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-05 20:09:49 +0530

    session grid added

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 // Sidebar.js
 import React from 'react';
-import { Box, Icon, Typography } from '@mui/material';
+import { Box, Icon, Typography, Card, CardContent, Button } from '@mui/material';
 import BusinessIcon from '@mui/icons-material/Business';
 
 
@@ -8,7 +8,18 @@ const Sidebar = () => {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
     //use this to make it mobile responsive
+
+    const cardsData=[
+    { title: 'Upload Speed', content: '5.2 MBPS' },
+    { title: 'Download Speed', content: '104.7 MBPS' },
+    { title: 'Session Data', content: '450.7 MB' },
+    { title: 'Nearest Server', content: 'Kolkata, India' },
+    { title: 'Current ISP Provider', content: 'Bharti Airtel' },
+    {title: 'Session Status', content:'Inactive'},
+    ]
+
   return (
+    
     console.log(windowWidth, windowHeight),
     <Box sx={{ width: "20%", height: '100vh', backgroundColor: '#353839', flexShrink: 0, padding:'16px' }}>
     {/**Icon and Product Name */}
@@ -18,6 +29,33 @@ const Sidebar = () => {
         NetPeek-Network Monitoring 
       </Typography>
       </Box>
+
+    {/**Other KPI's */}
+    {cardsData.map((card) => (
+  <Card sx={{ backgroundColor: '#2D2F30', marginBottom: '16px' }} key={card.title}>
+    <CardContent>
+      <Typography variant="h6" component="h2" gutterBottom style={{ color: 'white' }}>
+        {card.title}
+      </Typography>
+      {card.title === 'Session Status' ? (
+  card.content === 'Active' ? (
+    <Button variant="contained" style={{ backgroundColor: 'green', color: 'white' }}>
+      {card.content}
+    </Button>
+  ) : (
+    <Button variant="contained" style={{ backgroundColor: 'red', color: 'white' }}>
+      {card.content}
+    </Button>
+  )
+) : (
+  <Typography variant="h4" component="h2" gutterBottom style={{ color: 'white' }}>
+    {card.content}
+  </Typography>
+)}
+
+    </CardContent>
+  </Card>
+))}
 
     {/**Other KPI's */}
     
---
+++ client/src/components/charts/sessionGrid.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { DataGrid} from '@mui/x-data-grid';
+import { Box } from '@mui/system';
+
+const SessionGrid = () => {
+    const columns = [
+        {
+          field: 'id',
+          headerName: 'ID',
+          width: 90,
+        },
+        {
+          field: 'sessionId',
+          headerName: 'Session ID',
+          width: 180,
+          editable: true,
+        },
+        {
+          field: 'DataUsage',
+          headerName: 'Data Usage',
+          width: 160,
+          editable: true,
+        },
+        {
+          field: 'duration',
+          headerName: 'Duration',
+          type: 'number',
+          width: 160,
+          editable: true,
+        },
+      ];
+    
+
+  const rows = [
+        { id: 1, sessionId: 'ABC12345', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 2, sessionId: 'DEF67890', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 3, sessionId: 'GHI12345', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 4, sessionId: 'JKL67890', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 5, sessionId: 'MNO12345', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 6, sessionId: 'PQR67890', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 7, sessionId: 'STU12345', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 8, sessionId: 'VWX67890', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },
+        { id: 9, sessionId: 'YZA12345', DataUsage: Math.random() * 1000 + 100, duration: Math.floor(Math.random() * 60) + 1 },      
+  ];
+
+  return (
+    <Box sx={{ height: '100%', width: '120%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        pageSizeOptions={[5]}
+        style={{ color: 'white' }}
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+};
+
+export default SessionGrid;
