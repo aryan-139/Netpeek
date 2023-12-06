c5ca1d4491e8547b0591e334b8d9945792a61a55
Aryan Raj <aryanraj2k25@gmail.com>
2023-12-06 17:29:15 +0530

    minor fixes

--- server/routes/networkSpeed.js
+++ server/routes/networkSpeed.js
@@ -4,25 +4,38 @@ const NetworkSpeed = require('network-speed');
 const testNetworkSpeed = new NetworkSpeed();
 
 async function getNetworkDownloadSpeed() {
+    try {
     const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
     const fileSizeInBytes = 500000;
     const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
     return speed;
+    } catch (error) {
+        console.error('Error fetching download speed:', error);
+        throw new Error('BAD_CONNECTION');
+    }
+    
 }
 
 async function getNetworkUploadSpeed() {
-    const options = {
-        hostname: 'www.google.com',
-        port: 80,
-        path: '/catchers/544b09b4599c1d0200000289',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-    const fileSizeInBytes = 2000000;
-    const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-    return speed;
+    try {
+        const options = {
+            hostname: 'www.google.com',
+            port: 80,
+            path: '/catchers/544b09b4599c1d0200000289',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+        const fileSizeInBytes = 2000000;
+        const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+        return speed;
+        
+    } catch (error) {
+        console.error('Error fetching download speed:', error);
+        throw new Error('BAD_CONNECTION');
+    }
+    
 }
 
 module.exports = { getNetworkDownloadSpeed, getNetworkUploadSpeed };
